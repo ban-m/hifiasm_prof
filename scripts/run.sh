43342daf0fae127852fdc34c965d14df87695bd7
Bansho Masutani <banmasutani@gmail.com>
2022-05-11 12:24:06 +0900

    update

---
+++ scripts/run.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -ue
+OUTPUT=${PWD}/data
+GENOMESIZE=5000000
+DIVRATE=0.00002
+COVERAGE=40
+READLEN=20000
+STDDEV=1000
+RESULT=${PWD}/result
+for i in `seq 0 10`
+do
+    SEED=${RANDOM}
+    OUTDIR=${OUTPUT}/${SEED}
+    mkdir -p ${OUTDIR}
+    cargo run --release --bin syn_genome -- ${RANDOM} ${OUTDIR} ${GENOMESIZE} ${DIVRATE} \
+          ${COVERAGE} ${READLEN} ${STDDEV}
+    ASMDIR=${RESULT}/${SEED}
+    READS=${OUTDIR}/reads.fa
+    mkdir -p ${ASMDIR}
+    hifiasm -t56 --h-cov ${COVERAGE} -o ${ASMDIR}/asm ${READS} 2> ${ASMDIR}/log > ${ASMDIR}/out
+done
+
